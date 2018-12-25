ab1bd98d4c9df23c4e598b95df970bf1328a5ba1
TomohiroUmestu <32347348+TomohiroUmestu@users.noreply.github.com>
2018-12-26 01:19:46 +0900

    TomohiroUmestu
    upload kadai

--- kadai5.m
+++ kadai5.m
@@ -3,7 +3,7 @@
 % 下記はサンプルプログラムである．
 % 課題作成にあたっては「Lenna」以外の画像を用いよ．
 
-ORG=imread('Lenna.png'); % 原画像の入力
+ORG=imread('kadai2.jpg'); % 原画像の入力
 ORG = rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
 imagesc(ORG); colormap(gray); colorbar;
 pause;
@@ -32,3 +32,5 @@ end;
 IMG = ORG > max_thres;
 imagesc(IMG); colormap(gray); colorbar;
 pause;
+
+disp(sigma1)
